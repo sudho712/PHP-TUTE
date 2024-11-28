8000e55d38de3615f716a46f0f0e44ccd5a17d7f
Sudhir Kumar <sudho712@gmail.com>
2024-11-28 18:27:07 +0530

    php date function

---
+++ Date/simpleDate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Simple Date Function</h1>
+    <?php
+
+    echo "Simple date Function ";
+
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("d/m/y") . "<br>";
+
+    echo "Simple date Function ";
+
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+
+    echo "Current Day ";
+    echo "Today is " . date("l");
+    ?>
+
+</body>
+
+</html>
